e63c38a6514a8733dc4f751fb73c1ac2e68d43c6
bleonard <bodie.dev@gmail.com>
2014-03-05 17:05:30 -0600

    setup api

--- api/endpoints/contact.php
+++ api/endpoints/contact.php
@@ -5,8 +5,9 @@ $app->get($contact, function () use ($app) {
 
 		$app->response()->header('Content-Type', 'application/json');
 
-		$data = R::getAll("user");
-		$callback = array("status" => ['success'=>'true'], "data" => $data);
+		$data = R::find('user');
+		$data = R::exportAll($data); 
 
+		$callback = array("status" => ['success'=>'true'], "data" => $data);
 		echo json_encode($callback);
 });
